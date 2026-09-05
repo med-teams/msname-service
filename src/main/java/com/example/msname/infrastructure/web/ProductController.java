e9261a0065ec116607dfcef414e03baa12612c0b
MohamedElhanchir <elhanchirmohamed349@gmail.com>
2026-09-05 21:07:25 +0100

    test

--- src/main/java/com/example/msname/infrastructure/web/ProductController.java
+++ src/main/java/com/example/msname/infrastructure/web/ProductController.java
@@ -4,6 +4,8 @@ import com.example.commons.api.dto.ApiResponse;
 import com.example.msname.application.ProductService;
 import com.example.msname.application.dto.ProductRequest;
 import com.example.msname.application.dto.ProductResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     public ProductController(ProductService productService) {
         this.productService = productService;
@@ -20,16 +23,19 @@ public class ProductController {
 
     @PostMapping
     public ApiResponse<ProductResponse> create(@RequestBody ProductRequest request) {
+        log.info("Received request to create product: {}", request);
         return ApiResponse.ok(productService.create(request));
     }
 
     @GetMapping("/{id}")
     public ApiResponse<ProductResponse> getById(@PathVariable Long id) {
+        log.info("Received request to get product with ID: {}", id);
         return ApiResponse.ok(productService.getById(id));
     }
 
     @GetMapping
     public ApiResponse<List<ProductResponse>> getAll() {
+        log.info("Received request to get all products");
         return ApiResponse.ok(productService.getAll());
     }
 }
